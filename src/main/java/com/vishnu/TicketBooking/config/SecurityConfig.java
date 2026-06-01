c6839a843ad0c0711c2536e33551458311960c44
Vishnu4050 <venkatavishnu50@gmail.com>
2026-06-01 15:22:31 +0530

    Fixed HTTPS forwarding for Swagger

--- src/main/java/com/vishnu/TicketBooking/config/SecurityConfig.java
+++ src/main/java/com/vishnu/TicketBooking/config/SecurityConfig.java
@@ -79,6 +79,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**"
+
                         ).permitAll()
 
 
