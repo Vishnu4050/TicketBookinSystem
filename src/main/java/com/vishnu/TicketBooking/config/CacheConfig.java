16748a6da4a7a0aabc5ede212351c49c88814af9
Vishnu4050 <venkatavishnu50@gmail.com>
2026-06-04 15:59:57 +0530

    redis fix 5

--- src/main/java/com/vishnu/TicketBooking/config/CacheConfig.java
+++ src/main/java/com/vishnu/TicketBooking/config/CacheConfig.java
@@ -1,8 +1,5 @@
 package com.vishnu.TicketBooking.config;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,22 +13,11 @@ public class CacheConfig {
 
     @Bean
     public RedisCacheConfiguration cacheConfiguration() {
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        mapper.activateDefaultTyping(
-                BasicPolymorphicTypeValidator.builder()
-                        .allowIfSubType(Object.class)
-                        .build(),
-                ObjectMapper.DefaultTyping.NON_FINAL,
-                JsonTypeInfo.As.PROPERTY
-        );
-
         return RedisCacheConfiguration.defaultCacheConfig()
                 .serializeValuesWith(
                         RedisSerializationContext.SerializationPair
                                 .fromSerializer(
-                                        new GenericJackson2JsonRedisSerializer(mapper)
+                                        new GenericJackson2JsonRedisSerializer()
                                 )
                 );
     }
